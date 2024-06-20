9aaa10db91d482f15b7925a681cbeb88e143cc24
Eric Ho <xeric.hox@gmail.com>
2024-06-20 10:57:39 -0700

    use tab to cycle autocomplete

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- change keymap to find from cwd
+    { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (cwd)" },
+  },
+}
